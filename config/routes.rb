04414d5172f728d96e826167f5e04fdc6fed9f80
lidijarad <radovanovic.lidija22@gmail.com>
2022-01-24 17:29:28 +0200

    link diff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Regressions::Engine.routes.draw do
-  get '/:id', to: 'versions#show'
+  get '/:id', to: 'versions#show', as: "version"
   root to: 'versions#index'
 end
