7a6fc15f21175db72b7567078663c62c37f74c21
lidijarad <radovanovic.lidija22@gmail.com>
2022-01-24 16:40:23 +0200

    add individual view for regression changes

--- app/controllers/regressions/versions_controller.rb
+++ app/controllers/regressions/versions_controller.rb
@@ -4,8 +4,24 @@ require_dependency 'regressions/application_controller'
 
 module Regressions
   class VersionsController < ApplicationController
+
     def index
       @versions = PaperTrail::Version.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+      @versions = @versions.where(item_type: params[:type]) if params[:type]
+      @versions = @versions.where(item_id: params[:id]) if params[:id]
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @versions }
+      end
+    end
+
+    def show
+      @version = PaperTrail::Version.find(params[:id])
+    end
+
+    def permissible_params
+      params.permit(:id, :type)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Regressions::Engine.routes.draw do
+  get '/:id', to: 'versions#show'
   root to: 'versions#index'
 end
